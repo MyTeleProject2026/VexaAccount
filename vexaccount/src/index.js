8df9e84e54df1fe890b73a51093faebce51d967d
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-08-08 07:37:38 +0630

    Update index.js

--- vexaccount/src/index.js
+++ vexaccount/src/index.js
@@ -22,10 +22,8 @@ app.use(cookieParser());
 
 // CORS – allow all Vexa apps origins
 const allowedOrigins = [
-  process.env.FRONTEND_USER_URL || 'https://vexastore.onrender.com', 'https://vexatrade-6nhs.onrender.com',
+  process.env.FRONTEND_USER_URL || 'https://vexastore.onrender.com', 'https://vexatrade-6nhs.onrender.com', 'https://vexatrade-v.2bd.net', 'https://www.vexatrade-v.2bd.net',
   process.env.FRONTEND_ADMIN_URL || 'https://vexastore-admin.onrender.com', 'https://admin.vexatrade-v.2bd.net',
-  'https://vexatrade-v.2bd.net',
-  'https://www.vexatrade-v.2bd.net',
   'https://vexawallet.onrender.com',
   'https://vexabrowser.onrender.com',
   'http://localhost:5173',
